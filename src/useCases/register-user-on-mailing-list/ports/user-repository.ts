c61102e3a5edb17e75786fdfedae78be04dea4f2
Andrey Evyk <andreyevyk13@gmail.com>
2022-08-11 23:16:27 -0300

    feat: user repository interface

---
+++ src/useCases/register-user-on-mailing-list/ports/user-repository.ts
@@ -0,0 +1,8 @@
+import { UserDto } from '../user-dto'
+
+export interface UserRepository {
+  add(user: UserDto): Promise<void>
+  findUserByEmail(email:string): Promise<UserDto>
+  findAllUsers(): Promise<UserDto[]>
+  exists(user:UserDto): Promise<boolean>
+}
