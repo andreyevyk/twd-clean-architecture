a902bac65eaf4c3a1b95aced842ce0f279330ec3
Andrey Evyk <andreyevyk13@gmail.com>
2022-08-13 23:33:33 -0300

    feat: validate emails with empty domain part

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -7,11 +7,11 @@ export class Email {
       return false
     }
     const [local, domain] = email.split('@')
-    if (local.length > 64) {
+    if (local.length > 64 || local.length === 0) {
       return false
     }
 
-    if (domain.length > 255 || local.length === 0) {
+    if (domain.length > 255 || domain.length === 0) {
       return false
     }
     return true
