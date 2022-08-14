52e2a0da0d96dc0446a0a2bcec61c2cb6c2589eb
Andrey Evyk <andreyevyk13@gmail.com>
2022-08-13 23:25:34 -0300

    test: validate emaiol with more than 320 chars

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -25,4 +25,9 @@ describe('Email validation', () => {
     const email = 'l'.repeat(65) + '@mail.com'
     expect(Email.validate(email)).toBeFalsy()
   })
+
+  test('should not accept strings larget than 320 chars', () => {
+    const email = 'l'.repeat(64) + '@' + 'd'.repeat(128) + '.' + 'd'.repeat(127)
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })
