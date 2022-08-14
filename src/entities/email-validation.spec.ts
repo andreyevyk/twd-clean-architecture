e5f11c082969f2d3d533e99eecc6134d25c9b0e5
Andrey Evyk <andreyevyk13@gmail.com>
2022-08-13 23:59:51 -0300

    test: validate emails with local part with two dots and ending dot

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -55,4 +55,12 @@ describe('Email validation', () => {
     const email = 'any email@mail.com'
     expect(Email.validate(email)).toBeFalsy()
   })
+  test('should not accept local part with two dots char', () => {
+    const email = 'any..email@mail.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
+  test('should not accept local part with ending dot', () => {
+    const email = 'any.@mail.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })
