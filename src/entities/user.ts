29ed0200056f04a1046913a0365c916b4b75096b
Andrey Evyk <andreyevyk13@gmail.com>
2022-08-17 23:52:10 -0300

    feat: create valid user

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,4 +1,4 @@
-import { Either, left } from '../shared/either'
+import { Either, left, right } from '../shared/either'
 import { Email } from './email'
 import { InvalidEmailError } from './errors/invalid-email-error'
 import { InvalidNameError } from './errors/invalid-name-error'
@@ -6,6 +6,14 @@ import { Name } from './name'
 import { UserDto } from './user-dto'
 
 export class User {
+  public readonly email: Email
+  public readonly name: Name
+
+  private constructor (name: Name, email: Email) {
+    this.email = email
+    this.name = name
+  }
+
   static create (userData: UserDto): Either<InvalidNameError | InvalidEmailError, User> {
     const nameOrError = Name.create(userData.name)
     if (nameOrError.isLeft()) {
@@ -15,5 +23,10 @@ export class User {
     if (emailOrError.isLeft()) {
       return left(new InvalidEmailError())
     }
+
+    const name: Name = nameOrError.value as Name
+    const email: Email = emailOrError.value as Email
+
+    return right(new User(name, email))
   }
 }
