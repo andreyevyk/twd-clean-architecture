0cec8f31659d60fbbba1a21c6c8fafef274288f5
Andrey Evyk <andreyevyk13@gmail.com>
2022-08-24 12:36:41 -0300

    test: don't add user with invalid email

--- src/useCases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ src/useCases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,4 +1,6 @@
+import { InvalidNameError } from '../../entities/errors/invalid-name-error'
 import { UserDto } from '../../entities/user-dto'
+import { left } from '../../shared/either'
 import { UserRepository } from './ports/user-repository'
 import { RegisterUserOnMailingList } from './register-user-on-mailing-list'
 import { InMemoryUserRepository } from './repository/in-memory-user-repository'
@@ -18,5 +20,21 @@ describe('Register user on mailing list use case', () => {
 
     expect(user.name).toBe('any_name')
     expect(response.value.name).toBe('any_name')
+  })
+
+  test('should not add user with invalid email', async () => {
+    const users: UserDto[] = []
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+
+    const name = 'any_name'
+    const invalidEmail = 'invalid_email'
+
+    const response = await useCase.perform({ name, email: invalidEmail })
+
+    const user = await repo.findUserByEmail('any@email.com')
+
+    expect(user).toBe(null)
+    expect(response).toEqual(left(new InvalidNameError()))
   })
 })
